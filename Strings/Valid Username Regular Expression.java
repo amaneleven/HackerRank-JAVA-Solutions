d4d350751d88a7838a490d7ea1f6a4aed4325d08
Aman Eleven <hackerr647@gmail.com>
2022-03-06 13:08:15 +0530

    Create Valid Username Regular Expression.java

---
+++ Strings/Valid Username Regular Expression.java
@@ -0,0 +1,9 @@
+//just replace  null   with this exprsssion return at the end. that's it .. run it
+
+
+public static final String regularExpression ="^[a-zA-Z]\\w{7,29}$";
+
+
+
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven

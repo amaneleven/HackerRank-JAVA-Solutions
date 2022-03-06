03b7200c03425c1508ae824cf62a6b1cb32d3cb4
Aman Eleven <hackerr647@gmail.com>
2022-03-06 15:13:19 +0530

    Create Java 1D Array.java

---
+++ Data Structures/Java 1D Array.java
@@ -0,0 +1,13 @@
+ Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[]a = new int[n];
+        for(int i=0;i<n;i++)
+        {
+         
+        a[i]=scan.nextInt();
+        }
+        scan.close();
+
+
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
